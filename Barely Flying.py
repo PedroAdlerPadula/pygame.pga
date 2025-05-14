7744f110319061593ce2ac828e0093f7c7cd7b4e
Pedro Adler Padula <pedroap3@al.insper.edu.br>
2025-05-13 21:38:45 -0300

    tela inicial corrigida

--- Barely Flying.py
+++ Barely Flying.py
@@ -1,4 +1,4 @@
-import pygame 
+import pygame
 import random
 from math import * 
 
@@ -15,5 +15,20 @@ pygame.display.set_caption("Barely Flying")
 clock = pygame.time.Clock()
 # Load images
 
-background_image = pygame.image.load("Backgorun1.png")
+background_image = pygame.image.load("assets/background/Background1.png")
 background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
+background_rect = background_image.get_rect()
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((255 ,255 ,255))
+    screen.blit(background_image, background_rect)
+
+    pygame.display.flip()
+    pygame.display.update()
+
+pygame.quit()
